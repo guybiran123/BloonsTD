7e191a669fec57061ea151e46e27642930c7e5d8
Guy Biran <guybiran123@gmail.com>
2024-01-29 01:40:13 +0200

    reorganizing tower members

--- gameplay/towers/tower.py
+++ gameplay/towers/tower.py
@@ -1,8 +1,9 @@
 from utils.point import Point
 from gameplay.game_map import GameMap
+from utils.drawable import Drawable
 
 
-class Tower:
+class Tower(Drawable):
     """
     A father class that represents a tower in the game.
     """
@@ -10,21 +11,23 @@ class Tower:
         self,
         position: Point,
         direction: float,
+        image: str,
         cost: int,
         sell_cost: int,
-        shooting_speed: float,
-        shooting_damage: int,
+        shot_speed: int,
+        shot_damage: int,
+        shooting_speed: int,
         range_radius: int,
         is_pressed: bool,
         is_dragged: bool,
         game_map: GameMap
     ):
-        self.__position = position
-        self.__direction = direction
+        super().__init__(position, direction, image)
         self.__cost = cost
         self.__sell_cost = sell_cost
+        self.__shot_speed = shot_speed
+        self.__shot_damage = shot_damage
         self.__shooting_speed = shooting_speed
-        self.__shooting_damage = shooting_damage
         self.__range_radius = range_radius
         self.__is_pressed = is_pressed
         self.__is_dragged = is_dragged
@@ -36,11 +39,14 @@ class Tower:
     def get_sell_cost(self) -> int:
         return self.__sell_cost
 
+    def get_shot_speed(self):
+        return self.__shot_speed
+
+    def get_shot_damage(self):
+        return self.__shot_damage
+
     def get_shooting_speed(self):
         return self.__shooting_speed
-
-    def get_shooting_damage(self):
-        return self.__shooting_damage
 
     def get_range_radius(self):
         return self.__range_radius
