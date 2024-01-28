a4f4e858f95698d7ad746ccc3dc7f2e65a424713
Guy Biran <guybiran123@gmail.com>
2024-01-29 01:20:39 +0200

    adding getters and setters for tower

---
+++ gameplay/game_map.py
@@ -0,0 +1,6 @@
+
+class GameMap:
+    def __init__(self):
+        self.__balloons = []
+        self.__towers = []
+        self.__shots = []
--- gameplay/towers/tower.py
+++ gameplay/towers/tower.py
@@ -1,7 +1,10 @@
+from utils.point import Point
+from gameplay.game_map import GameMap
+
 
 class Tower:
     """
-    A class that represents a tower in the game.
+    A father class that represents a tower in the game.
     """
     def __init__(
         self,
@@ -14,7 +17,7 @@ class Tower:
         range_radius: int,
         is_pressed: bool,
         is_dragged: bool,
-        game_map
+        game_map: GameMap
     ):
         self.__position = position
         self.__direction = direction
@@ -27,14 +30,30 @@ class Tower:
         self.__is_dragged = is_dragged
         self.__game_map = game_map
 
-    def get_position(self) -> Point:
-        return self.__position
+    def get_cost(self) -> int:
+        return self.__cost
 
-    def set_position(self, position: Point) -> None:
-        self.__position = position
+    def get_sell_cost(self) -> int:
+        return self.__sell_cost
 
-    def get_direction(self) -> float:
-        return self.__direction
+    def get_shooting_speed(self):
+        return self.__shooting_speed
 
-    def set_direction(self, direction: float) -> None:
-        self.__direction = direction
+    def get_shooting_damage(self):
+        return self.__shooting_damage
+
+    def get_range_radius(self):
+        return self.__range_radius
+
+    def get_is_pressed(self):
+        return self.__is_pressed
+
+    def set_is_pressed(self, is_pressed: bool) -> None:
+        self.__is_pressed = is_pressed
+
+    def get_is_dragged(self):
+        return self.__is_dragged
+
+    def set_is_dragged(self, is_dragged) -> None:
+        self.__is_dragged = is_dragged
+
