1d1239903a1330b2833b3f2d40d97560f78fa36b
Guy Biran <guybiran123@gmail.com>
2024-01-28 23:34:20 +0200

    Adding some getters and setters to tower

--- tower.py
+++ tower.py
@@ -1,7 +1,23 @@
 from point import Point
 
+
 class Tower:
-    def __init__(self, position: Point, direction, cost, sell_cost, shooting_speed, shooting_damage, range_radius, is_pressed, is_dragged, game_map):
+    """
+    A class that represents a tower in the game.
+    """
+    def __init__(
+        self,
+        position: Point,
+        direction: float,
+        cost: int,
+        sell_cost: int,
+        shooting_speed: float,
+        shooting_damage: int,
+        range_radius: int,
+        is_pressed: bool,
+        is_dragged: bool,
+        game_map
+    ):
         self.__position = position
         self.__direction = direction
         self.__cost = cost
@@ -12,3 +28,15 @@ class Tower:
         self.__is_pressed = is_pressed
         self.__is_dragged = is_dragged
         self.__game_map = game_map
+
+    def get_position(self) -> Point:
+        return self.__position
+
+    def set_position(self, position: Point) -> None:
+        self.__position = position
+
+    def get_direction(self) -> float:
+        return self.__direction
+
+    def set_direction(self, direction: float) -> None:
+        self.__direction = direction
